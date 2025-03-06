bf2f1a8758027eadbafcd2c8aef54792d8e60294
FarzadHM7 <farzadmoghadam085@gmail.com>
2025-03-06 21:40:16 +0330

    new change

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     cout<<"Hello World!"<<endl;
-    cout<<"Farzad Hallaj Moghaddam"<<endl;
-    cout<<"403101745";
+//    cout<<"Farzad Hallaj Moghaddam"<<endl;
+//    cout<<"403101745";
     return 0;
 }
